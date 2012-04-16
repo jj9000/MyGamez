04530dea7160929f9ce09612e838b6607282f1b4
av_jui <av_jui@freevdr>
2012-04-16 14:04:31 +0200

    added ' to helper

--- lib/Helper.py
+++ lib/Helper.py
@@ -2,7 +2,7 @@ import os
 import re
 
 def replace_all(text):
-    dic = {'...':'', ' & ':' ', ' = ': ' ', '?':'', '$':'s', ' + ':' ', '"':'', ',':'', '*':'', '.':'', ':':''}	
+    dic = {'...':'', ' & ':' ', ' = ': ' ', '?':'', '$':'s', ' + ':' ', '"':'', ',':'', '*':'', '.':'', ':':'', "'":''}	
     for i, j in dic.iteritems():
        text = text.replace(i, j)
     return text
